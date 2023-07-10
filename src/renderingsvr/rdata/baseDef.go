49913494a43148fd765459d134adcea68d914fa2
vilyLei <vily313@126.com>
2023-07-10 20:12:03 +0800

    fix: update some info and some codes.

--- src/renderingsvr/rdata/baseDef.go
+++ src/renderingsvr/rdata/baseDef.go
@@ -6,7 +6,16 @@ import (
 )
 
 // go mod init renderingsvr.com/rdata
-
+type RTMaterialNode struct {
+	ModelName      string     `json:"modelName"`
+	UVScales       [2]float64 `json:"uvScales"`
+	Type           string     `json:"type"`
+	Color          uint       `json:"color"`
+	Specular       float64    `json:"specular"`
+	Metallic       float64    `json:"metallic"`
+	Roughness      float64    `json:"roughness"`
+	NormalStrength float64    `json:"normalStrength"`
+}
 type RTEnvNode struct {
 	Path       string  `json:"path"`
 	Type       string  `json:"type"`
@@ -19,7 +28,7 @@ type RTOutputNode struct {
 	OutputType    string `json:"outputType"`
 	Resolution    [2]int `json:"resolution"`
 	BGTransparent int    `json:"bgTransparent"`
-	BGColor       int    `json:"bgColor"`
+	BGColor       uint   `json:"bgColor"`
 }
 type RTRCameraNode struct {
 	Type      string      `json:"type"`
@@ -33,9 +42,10 @@ type RTRenderingNode struct {
 	Unit    string `json:"unit"`
 	Version int64  `json:"version"`
 
-	Camera RTRCameraNode `json:"camera"`
-	Output RTOutputNode  `json:"output"`
-	Env    RTEnvNode     `json:"env"`
+	Camera    RTRCameraNode    `json:"camera"`
+	Output    RTOutputNode     `json:"output"`
+	Env       RTEnvNode        `json:"env"`
+	Materials []RTMaterialNode `json:"materials"`
 }
 
 func (self *RTRenderingNode) setFromJson(rnodeJsonStr string) {
